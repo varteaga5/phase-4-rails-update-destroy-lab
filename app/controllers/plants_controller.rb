00fcdfecab5658b93f76dcde3179fa213bab0c19
vincent <vincent.arteaga@gmail.com>
2021-09-21 15:00:10 -0700

    commit tree 7886fd9fc5f686640d1e4910ffbfa84802281f14

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -11,16 +11,41 @@ class PlantsController < ApplicationController
     plant = Plant.find_by(id: params[:id])
     render json: plant
   end
-
+  
   # POST /plants
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  
+  def update
+    plant = Plant.find_by(id: params[:id])
+      plant.update(plant_params)
+      render json: plant
+  end
 
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
 end
+
+
+
+#   def update 
+#     plant = Plant.find_by(id: params[:id])
+#     plant.update(plant_params)
+#     render json: plant
+#   end
+
+#   def destroy
+#     plant = Plant.find_by(id: params[:id])
+#     plant.destroy
+#     head :no_content
+#   end
